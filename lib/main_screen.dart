a2c645e90b022c51e9559a6c0886d39086aea609
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-06-16 00:10:31 +0900

    081. 옵션 메뉴

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -44,6 +44,31 @@ class _MainScreenState extends State<MainScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('나만의 웹브라우저'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          ),
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              print(value);
+            },
+            itemBuilder: (context) => [
+              const PopupMenuItem<String>(
+                value: 'https://www.google.com',
+                child: Text('구글'),
+              ),
+              const PopupMenuItem<String>(
+                value: 'https://www.naver.com',
+                child: Text('네이버'),
+              ),
+              const PopupMenuItem<String>(
+                value: 'https://www.kakao.com',
+                child: Text('카카오'),
+              ),
+            ],
+          )
+        ],
       ),
       body: WebViewWidget(controller: controller),
     );
